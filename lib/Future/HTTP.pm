4c4ea1feed99f831e5f15849b288787eac220690
Max Maischein <corion@corion.net>
2016-05-16 16:31:06 +0200

    Add documentation

--- lib/Future/HTTP.pm
+++ lib/Future/HTTP.pm
@@ -8,6 +8,24 @@ use feature 'signatures';
 
 Future::HTTP - provide the most appropriate HTTP client with a Future API
 
+=head1 SYNOPSIS
+
+    my $ua = HTTP::Future->new();
+    $ua->http_get('http://www.nethype.de/')->then(sub {
+        my( $body, $data ) = @_;
+        ...
+    })
+
+This module is a wrapper combining L<Future> with the API provided
+by L<AnyEvent::HTTP>. The backend used for the HTTP protocols
+depends on whether one of the event loops is loaded.
+
+=head2 Supported event loops
+
+Currently only L<HTTP::Tiny> and L<AnyEvent> are supported. Support
+is planned for L<LWP::UserAgent>, L<Mojolicious> and L<IO::Async>
+but has not materialized yet.
+
 =cut
 
 use vars qw($implementation @loops $VERSION);
@@ -18,12 +36,22 @@ $VERSION = '0.01';
     ['AnyEvent.pm'    => 'Future::HTTP::AnyEvent'],
     ['AE.pm'          => 'Future::HTTP::AnyEvent'],
     # POE support would be nice
-    # IO::Async support would be nice
+    # IO::Async support would be nice, using Net::Async::HTTP
     # LWP::UserAgent support would be nice
     
     # The fallback, will always catch due to loading Future::HTTP
     ['Future/HTTP.pm' => 'Future::HTTP::Tiny'],
 );
+
+=head1 METHODS
+
+=head2 C<< HTTP::Future->new() >>
+
+    my $ua = HTTP::Future->new();
+
+Creates a new instance of the HTTP client.
+
+=cut
 
 sub new($factoryclass, @args) {
     $implementation ||= $factoryclass->best_implementation();
@@ -54,6 +82,66 @@ sub best_implementation( $class, @candidates ) {
 };
 
 # We support the L<AnyEvent::HTTP> API first
+
+=head2 C<< $ua->http_get($url, %options) >>
+
+    $ua->http_get('http://example.com/',
+        headers => {
+            'Accept' => 'text/json',
+        },
+    )->then(sub {
+        my( $body, $headers ) = @_;
+        ...
+    });
+
+Retrieves the URL and returns the body and headers, like
+the function in L<AnyEvent::HTTP>.
+
+=head2 C<< $ua->http_head($url, %options) >>
+
+    $ua->http_head('http://example.com/',
+        headers => {
+            'Accept' => 'text/json',
+        },
+    )->then(sub {
+        my( $body, $headers ) = @_;
+        ...
+    });
+
+Retrieves the header of the URL and returns the headers,
+like the function in L<AnyEvent::HTTP>.
+
+=head2 C<< $ua->http_post($url, $body, %options) >>
+
+    $ua->http_post('http://example.com/api',
+        '{token:"my_json_token"}',
+        headers => {
+            'Accept' => 'text/json',
+        },
+    )->then(sub {
+        my( $body, $headers ) = @_;
+        ...
+    });
+
+Posts the content to the URL and returns the body and headers,
+like the function in L<AnyEvent::HTTP>.
+
+=head2 C<< $ua->http_request($method, $url, %options) >>
+
+    $ua->http_request('PUT' => 'http://example.com/api',
+        headers => {
+            'Accept' => 'text/json',
+        },
+        body    => '{token:"my_json_token"}',
+    )->then(sub {
+        my( $body, $headers ) = @_;
+        ...
+    });
+
+Posts the content to the URL and returns the body and headers,
+like the function in L<AnyEvent::HTTP>.
+
+=cut
 
 # We should support more APIs like HTTP::Tiny, later
 # See L<Future::HTTP::API::HTTPTiny>.
