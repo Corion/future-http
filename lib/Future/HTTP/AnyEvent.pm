39bed6a254a5455257ac7d737288d4e6713dcb98
Max Maischein <corion@corion.net>
2024-04-11 20:35:47 +0200

    Bump copyright year

--- lib/Future/HTTP/AnyEvent.pm
+++ lib/Future/HTTP/AnyEvent.pm
@@ -162,7 +162,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2016-2023 by Max Maischein C<corion@cpan.org>.
+Copyright 2016-2024 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
